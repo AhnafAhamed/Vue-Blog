54e96705e19912bc36a0c382d6e6cfcc9fd83965
AhnafAhamed <ahnafahamed4@gmail.com>
2022-05-22 09:36:34 +0530

    feat: add blog post and hero section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,84 @@
 <template>
   <div class="home">
+    <blog-post :post="welcomeScreen"/>
+    <blog-post :post="post" v-for="(post, index) in sampleBlogPosts" :key="index"/>
   </div>
+  
 </template>
 
 <script>
+import BlogPost from '../components/BlogPost.vue';
 export default {
   name: "Home",
-  components: {},
+  components: {BlogPost},
+  data() {
+    return {
+      welcomeScreen: {
+        title: "Welcome!",
+        blogPost:
+          "Weekly blog articles with all things programming including HTML, CSS, JavaScript and more. Register today to never miss a post!",
+        welcomeScreen: true,
+        photo: "coding",
+      },
+      sampleBlogPosts: [
+        {
+          title: "This is a sample",
+          blogHTML: "This is just a sample text",
+          blogCoverPhoto: "hello",
+        },
+        {
+          title: "This is a sample",
+          blogHTML: "This is just a sample text",
+          blogCoverPhoto: "hello",
+        },
+      ],
+    };
+  },
 };
 </script>
+
+
+<style lang="scss" scoped>
+.blog-card-wrap {
+  h3 {
+    font-weight: 300;
+    font-size: 28px;
+    margin-bottom: 32px;
+  }
+}
+
+.updates {
+  .container {
+    padding: 100px 25px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    @media (min-width: 800px) {
+      padding: 125px 25px;
+      flex-direction: row;
+    }
+
+    .router-button {
+      display: flex;
+      font-size: 14px;
+      text-decoration: none;
+      @media (min-width: 800px) {
+        margin-left: auto;
+      }
+    }
+
+    h2 {
+      font-weight: 300;
+      font-size: 32px;
+      max-width: 425px;
+      width: 100%;
+      text-align: center;
+      text-transform: uppercase;
+      @media (min-width: 800px) {
+        text-align: initial;
+        font-size: 40px;
+      }
+    }
+  }
+}
+</style>
