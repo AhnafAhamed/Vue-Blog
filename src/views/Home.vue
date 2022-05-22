3b8810af34dc36d839618ec8db3b732977000ca5
AhnafAhamed <ahnafahamed4@gmail.com>
2022-05-22 10:52:48 +0530

    feat: create blog cards

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,15 +2,24 @@
   <div class="home">
     <blog-post :post="welcomeScreen"/>
     <blog-post :post="post" v-for="(post, index) in sampleBlogPosts" :key="index"/>
+    <div class="blog-card-wrap">
+      <div class="container">
+        <h3>View More Recent Blogs</h3>
+        <div class="blog-cards">
+          <blog-card :post="post" v-for="(post, index) in sampleBlogCards" :key="index"/>
+        </div>
+      </div>
+    </div>
   </div>
   
 </template>
 
 <script>
+import BlogCard from '../components/BlogCard.vue';
 import BlogPost from '../components/BlogPost.vue';
 export default {
   name: "Home",
-  components: {BlogPost},
+  components: {BlogPost, BlogCard},
   data() {
     return {
       welcomeScreen: {
@@ -24,14 +33,20 @@ export default {
         {
           title: "This is a sample",
           blogHTML: "This is just a sample text",
-          blogCoverPhoto: "hello",
+          blogCoverPhoto: "beautiful-stories",
         },
         {
           title: "This is a sample",
           blogHTML: "This is just a sample text",
-          blogCoverPhoto: "hello",
+          blogCoverPhoto: "designed-for-everyone",
         },
       ],
+      sampleBlogCards: [
+        { blogTitle: "Blog 1", blogCoverPhoto: "stock-1", blogDate: "May 1 2021" },
+        { blogTitle: "Blog 2", blogCoverPhoto: "stock-2", blogDate: "May 3 2021" },
+        { blogTitle: "Blog 3", blogCoverPhoto: "stock-3", blogDate: "May 7 2021" },
+        { blogTitle: "Blog 4", blogCoverPhoto: "stock-4", blogDate: "May 9 2021" },
+      ]
     };
   },
 };
